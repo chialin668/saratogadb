ec08c90be01967ccf17d55d96e26a79bdc307438
Chialin Chou <chialin668@gmail.com>
2014-03-15 23:26:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Saratogadb::Application.config.secret_key_base = '35ac54fe1f2125940708bee215ef07b68abad1c6737c3d943f37c4b717528b23ec34da87f8748bc572c72c55a34978233d89d3aee00352064bfc185f3a84f215'
